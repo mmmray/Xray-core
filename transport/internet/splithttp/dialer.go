163bfbb947f2b00451a2467781874a1528f6d3e1
mmmray <142015632+mmmray@users.noreply.github.com>
2024-06-01 16:12:29 +0200

    refactor

--- transport/internet/splithttp/dialer.go
+++ transport/internet/splithttp/dialer.go
@@ -22,8 +22,8 @@ func init() {
     common.Must(internet.RegisterTransportDialer(protocolName, Dial))
 }
 
-func maybeWrapTls(ctx context.Context, conn net.Conn, dest net.Destination, streamSettings *internet.MemoryStreamConfig) (net.Conn, error) {
-	if config := tls.ConfigFromStreamSettings(streamSettings); config != nil {
+func maybeWrapTls(ctx context.Context, conn net.Conn, dest net.Destination, config *tls.Config) (net.Conn, error) {
+	if config != nil {
 		tlsConfig := config.GetTLSConfig(tls.WithDestination(dest))
 		if fingerprint := tls.GetFingerprint(config.Fingerprint); fingerprint != nil {
 			conn = tls.UClient(conn, tlsConfig, fingerprint)
@@ -42,9 +42,10 @@ func Dial(ctx context.Context, dest net.Destination, streamSettings *internet.Me
 	newError("dialing splithttp to ", dest).WriteToLog(session.ExportIDToError(ctx))
 
     transportConfiguration := streamSettings.ProtocolSettings.(*Config)
+    tlsConfig := tls.ConfigFromStreamSettings(streamSettings)
 
 	var requestURL url.URL
-	if config := tls.ConfigFromStreamSettings(streamSettings); config != nil {
+	if tlsConfig != nil {
         requestURL.Scheme = "https"
     } else {
         requestURL.Scheme = "http"
@@ -62,7 +63,7 @@ func Dial(ctx context.Context, dest net.Destination, streamSettings *internet.Me
             return nil, err
         }
 
-        return maybeWrapTls(ctx, conn, dest, streamSettings)
+        return maybeWrapTls(ctx, conn, dest, tlsConfig)
     }
 
     httpTransport := http.Transport{
@@ -98,9 +99,13 @@ func Dial(ctx context.Context, dest net.Destination, streamSettings *internet.Me
         return nil, err
     }
 
+    if downResponse.StatusCode != 200 {
+        return nil, newError("invalid status code on download:", downResponse.Status)
+    }
+
     uploadUrl := requestURL.String() + "?session=" + sessionId
 
-    uploadPipeReader, uploadPipeWriter := pipe.New(pipe.WithSizeLimit(128000))
+    uploadPipeReader, uploadPipeWriter := pipe.New(pipe.WithSizeLimit(1000000))
 
     go func() {
         // by offloading the uploads into a buffered pipe, multiple conn.Write
@@ -124,7 +129,7 @@ func Dial(ctx context.Context, dest net.Destination, streamSettings *internet.Me
                 break
             }
 
-            if resp.Status != "200 OK" {
+            if resp.StatusCode != 200 {
                 break
             }
         }
